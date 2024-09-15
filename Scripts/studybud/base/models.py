42fb2eea459006165d379a235b3e36aa5a3b86bd
Geino Gosey III <geino.gosey426@gmail.com>
2024-09-15 12:35:26 -0400

    Search

--- Scripts/studybud/base/models.py
+++ Scripts/studybud/base/models.py
@@ -17,15 +17,18 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
         return self.name
     
-    class Message(models.Model):
+class Message(models.Model):
         user = models.ForeignKey(User, on_delete=models.CASCADE)
         room = models.ForeignKey(Room, on_delete=models.CASCADE)
         body = models.TextField()
         updated = models.DateTimeField(auto_now=True)
-        Created = models.DateTimeField(auto_now_add=True)
+        created = models.DateTimeField(auto_now_add=True)
 
         def __str__(self):
             return self.body[0:50]
